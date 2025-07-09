085762273a46663424583b282f7e07366128acc6
libbieray <libbie@connectedeventgroup.com>
2025-07-09 17:13:23 +1000

    Update main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -151,7 +151,6 @@ def init_database(app):
                 is_active=True,
                 email_verified=True
             )
-            admin_user.set_password('CyberconAdmin2025!')
             admin_user.roles.append(admin_role)
             
             db.session.add(admin_user)
