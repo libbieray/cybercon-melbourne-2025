7c072cef859cfa37dd0e020ece9aeae92b5d953e
libbieray <libbie@connectedeventgroup.com>
2025-07-09 17:33:20 +1000

    Update main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -151,7 +151,7 @@ def init_database(app):
                 is_active=True,
                 email_verified=True
             )
-            admin_user.roles.append(admin_role)
+        admin_user.roles.append(admin_role)
         
             db.session.add(admin_user)
             db.session.commit()
